dd372305f32d47c1a16c3de419d5ee189ab90510
Egor <semovev@gmail.com>
2021-07-16 16:39:18 +0500

    add second video and script for this

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,13 @@ const h2 = document.querySelectorAll(".history__legend h2");
 const history_first = document.querySelector(".history__first");
 const history_second = document.querySelector(".history__second");
 
+//Для переключения слайдера видео
+const descrip_nav = document.querySelectorAll(".description__nav");
+const video_first = document.querySelector(".video_first");
+const video_second = document.querySelector(".video_second");
+const video_third = document.querySelector(".video_third");
+
+
 
 //Переключение между заголовками при нажатии
 h2.forEach(item =>{
@@ -18,6 +25,34 @@ h2.forEach(item =>{
                 }
             }else{
                 h2[i].classList.remove("legend__active");
+            }
+        }
+    });
+})
+
+
+//Слайдер видео про историю
+descrip_nav.forEach(nav => {
+    nav.addEventListener("click", () => {
+        for(var i = 0; i < descrip_nav.length; i++){
+            if(descrip_nav[i] == nav){
+                descrip_nav[i].classList.add("description__nav__active");
+                if(i == 0){
+                    video_first.style.opacity = 100;
+                    video_second.style.opacity = 0;
+                    video_third.style.opacity = 0;
+                }else if(i == 1){
+                    video_first.style.opacity = 0;
+                    video_second.style.opacity = 100;
+                    video_third.style.opacity = 0;
+                }else{
+                    video_first.style.opacity = 0;
+                    video_second.style.opacity = 0;
+                    video_third.style.opacity = 100;
+                }
+            }
+            else{
+                descrip_nav[i].classList.remove("description__nav__active");
             }
         }
     });
